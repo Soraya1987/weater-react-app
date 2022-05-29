6d1ee54d06065281a3d6bb0c3dc425461580a499
Soraya <soraya_mahmoudi95@yahoo.com>
2022-05-29 19:45:19 +0430

    changed Loader to Puff

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import Loader from "react-loader-spinner";
+import {Puff} from "react-loader-spinner";
 
 export default function Weather(props){
     function handleResponse(response){
@@ -11,7 +11,7 @@ export default function Weather(props){
     let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
     return(
-        <Loader type="puff"
+        <Puff 
                 color="orange"
                 height={100}
                 width={100}
